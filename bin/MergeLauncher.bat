34a3c4cfb0a64d0ce32c0708beac9894c5ff7b4c
MHGameWorkBase2 <mhgamework@gmail.com>
2011-04-14 20:19:51 +0200

    Almost there...
    Signed-off-by: MHGameWorkBase2 <mhgamework@gmail.com>

---
+++ bin/MergeLauncher.bat
@@ -0,0 +1,3 @@
+mkdir _Merged
+ILMerge.exe /out:_Merged\Launcher.exe /targetplatform:"v4, C:\Windows\Microsoft.NET\Framework\v4.0.30319" Launcher.exe Networking.dll Common.NetworkingVB.dll Common.Core.dll Game3DPlay.dll ICSharpCode.SharpZipLib.dll
+
