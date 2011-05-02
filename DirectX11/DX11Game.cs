f2606fb92b457d1fe9279f7bd1b253d1de08333b
MHGameWorkBase <mhgamework@gmail.com>
2011-05-03 01:32:15 +0200

    Some research done on DX11
    Signed-off-by: MHGameWorkBase <mhgamework@gmail.com>
    
    Former-commit-id: 8686ec51b55d6a5feebafff025c7f27b4d9c1816

---
+++ DirectX11/DX11Game.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using SharpDX;
+using SharpDX.D3DCompiler;
+using SharpDX.Direct3D;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using SharpDX.Windows;
+using Buffer = SharpDX.Direct3D11.Buffer;
+using Device = SharpDX.Direct3D11.Device;
+using Resource = SharpDX.Direct3D11.Resource;
+
+namespace DirectX11
+{
+    public class DX11Game
+    {
+        private SwapChain swapChain;
+        private RenderForm form;
+        private Texture2D backBuffer;
+        private RenderTargetView renderView;
+        public event Action GameLoopEvent;
+
+        public int FrameCount { get; private set; }
+
+        public DX11Game()
+        {
+
+        }
+
+        public DeviceContext Context { get; private set; }
+
+        public Device Device
+        { get; private set; }
+
+        public RenderForm Form
+        {
+            get { return form; }
+            set { form = value; }
+        }
+
+        public RenderTargetView RenderView
+        {
+            get { return renderView; }
+            set { renderView = value; }
+        }
+
+
+        private void GameLoop()
+        {
+            Context.ClearRenderTargetView(RenderView, new Color4(1.0f, 0.0f, 0.0f, 0.0f));
+
+            if (GameLoopEvent != null)
+                GameLoopEvent();
+
+            swapChain.Present(0, PresentFlags.None);
+            FrameCount++;
+        }
+
+        public void Run()
+        {
+            Form = new RenderForm("SharpDX - MiniTri Direct3D 11 Sample");
+
+            // SwapChain description
+            var desc = new SwapChainDescription()
+            {
+                BufferCount = 1,
+                ModeDescription =
+                    new ModeDescription(Form.ClientSize.Width, Form.ClientSize.Height,
+                                        new Rational(60, 1), Format.R8G8B8A8_UNorm),
+                IsWindowed = true,
+                OutputHandle = Form.Handle,
+                SampleDescription = new SampleDescription(8, 16),
+                SwapEffect = SwapEffect.Discard,
+                Usage = Usage.RenderTargetOutput,
+            };
+
+            SharpDX.Direct3D11.Device dev;
+            // Create Device and SwapChain
+            Device.CreateWithSwapChain(DriverType.Hardware, DeviceCreationFlags.Debug, desc, out dev, out swapChain);
+            Device = dev;
+            Context = Device.ImmediateContext;
+
+            Form.Resize += new System.EventHandler(form_Resize);
+
+            // Ignore all windows events
+            Factory factory = swapChain.GetParent<Factory>();
+            //factory.MakeWindowAssociation(form.Handle, WindowAssociationFlags.None);
+
+            // New RenderTargetView from the backbuffer
+            backBuffer = Resource.FromSwapChain<Texture2D>(swapChain, 0);
+            RenderView = new RenderTargetView(Device, backBuffer);
+
+        
+
+
+
+
+            // Main loop
+            RenderLoop.Run(Form, GameLoop);
+
+            // Release all resources
+            RenderView.Release();
+            backBuffer.Release();
+            Context.ClearState();
+            Context.Flush();
+            Device.Release();
+            Context.Release();
+            swapChain.Release();
+            factory.Release();
+
+        }
+
+
+        void form_Resize(object sender, System.EventArgs e)
+        {
+            // Disabled, this causes random crashes atm
+
+            return;
+            backBuffer.Release();
+            RenderView.Release();
+            try
+            {
+                var r = swapChain.ResizeBuffers(swapChain.Description.BufferCount, Form.ClientSize.Width, Form.ClientSize.Height,
+                                     swapChain.Description.ModeDescription.Format, (int)SwapChainFlags.None);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debugger.Break();
+            }
+
+
+            backBuffer = Resource.FromSwapChain<Texture2D>(swapChain, 0);
+            RenderView = new RenderTargetView(Device, backBuffer);
+            Context.OutputMerger.SetTargets(RenderView);
+            Context.Rasterizer.SetViewports(new Viewport(0, 0, Form.ClientSize.Width, Form.ClientSize.Height, 0.0f, 1.0f));
+        }
+    }
+}
--- Unit Tests/DirectX11/DirectX11Test.cs
+++ Unit Tests/DirectX11/DirectX11Test.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Reflection;
 using System.Threading;
 using System.Windows.Forms;
+using DirectX11;
 using NUnit.Framework;
 using SharpDX;
 using SharpDX.D3DCompiler;
@@ -223,12 +224,12 @@ namespace MHGameWork.TheWizards.Tests.DirectX11
             var dataPart1 = secondarySoundBuffer.Lock(0, capabilities.BufferBytes, LockFlags.EntireBuffer, out dataPart2);
 
             // Fill the buffer with some sound
-            int numberOfSamples = capabilities.BufferBytes/waveFormat.BlockAlign;
+            int numberOfSamples = capabilities.BufferBytes / waveFormat.BlockAlign;
             for (int i = 0; i < numberOfSamples; i++)
             {
-                double vibrato = Math.Cos(2*Math.PI*10.0*i/waveFormat.SampleRate);
-                short value = (short) (Math.Cos(2*Math.PI*(220.0 + 4.0*vibrato)*i/waveFormat.SampleRate)*16384);
-                    // Not too loud
+                double vibrato = Math.Cos(2 * Math.PI * 10.0 * i / waveFormat.SampleRate);
+                short value = (short)(Math.Cos(2 * Math.PI * (220.0 + 4.0 * vibrato) * i / waveFormat.SampleRate) * 16384);
+                // Not too loud
                 dataPart1.Write(value);
                 dataPart1.Write(value);
             }
@@ -240,6 +241,97 @@ namespace MHGameWork.TheWizards.Tests.DirectX11
             secondarySoundBuffer.Play(0, SharpDX.DirectSound.PlayFlags.Looping);
 
             Application.Run(form);
+        }
+
+        [Test]
+        public void TestDX11Game()
+        {
+            var game = new DX11Game();
+           
+            VertexShader vertexShader = null;
+            ShaderBytecode pixelShaderByteCode = null;
+            InputLayout layout = null;
+            Buffer vertices = null;
+            PixelShader pixelShader = null;
+
+            RasterizerState rasterizerState = null;
+            game.GameLoopEvent += delegate
+                                  {
+                                      var device = game.Device;
+                                      var Context = device.ImmediateContext;
+                                      if (game.FrameCount == 0)
+                                      {
+                                          byte[] shaderCode = null;
+                                          using (var strm = Assembly.GetExecutingAssembly().GetManifestResourceStream("MHGameWork.TheWizards.Tests.DirectX11.MiniTri.fx"))
+                                          {
+                                              shaderCode = new byte[(int)strm.Length];
+                                              strm.Read(shaderCode, 0, (int)strm.Length);
+                                          }
+
+                                          var vertexShaderByteCode = ShaderBytecode.Compile(shaderCode, "VS", "vs_4_0", ShaderFlags.None,
+                                                            EffectFlags.None);
+                                          vertexShader = new VertexShader(device, vertexShaderByteCode);
+
+                                          pixelShaderByteCode = ShaderBytecode.Compile(shaderCode, "PS", "ps_4_0", ShaderFlags.None,
+                                                                                       EffectFlags.None);
+                                          pixelShader = new PixelShader(device, pixelShaderByteCode);
+
+                                          // Layout from VertexShader input signature
+                                          layout = new InputLayout(device, ShaderSignature.GetInputSignature(vertexShaderByteCode), new[] { 
+                                                                                                                                              new InputElement("POSITION",0,Format.R32G32B32A32_Float,0,0),
+                                                                                                                                              new InputElement("COLOR",0,Format.R32G32B32A32_Float,16,0)
+                                                                                                                                          });
+
+                                          // Write vertex data to a datastream
+                                          var stream = new DataStream(32 * 3, true, true);
+                                          stream.WriteRange(new[]
+                                                {
+                                                    new Vector4(0.0f, 0.5f, 0.5f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+                                                    new Vector4(0.5f, -0.5f, 0.5f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+                                                    new Vector4(-0.5f, -0.5f, 0.5f, 1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f)
+                                                });
+                                          stream.Position = 0;
+
+
+
+                                          // Instantiate Vertex buiffer from vertex data
+                                          vertices = new Buffer(device, stream, new BufferDescription()
+                                                                                    {
+                                                                                        BindFlags = BindFlags.VertexBuffer,
+                                                                                        CpuAccessFlags = CpuAccessFlags.None,
+                                                                                        OptionFlags = ResourceOptionFlags.None,
+                                                                                        SizeInBytes = 32 * 3,
+                                                                                        Usage = ResourceUsage.Default,
+                                                                                        StructureByteStride = 0
+                                                                                    });
+
+                                          stream.Release();
+
+                                          var rasterDesc = new RasterizerStateDescription();
+                                          rasterDesc.CullMode = CullMode.Back;
+                                          rasterDesc.FillMode = FillMode.Solid;
+                                          rasterDesc.IsMultisampleEnabled = true;
+                                          rasterizerState = new RasterizerState(device, rasterDesc);
+
+                                      }
+
+                                      // Prepare All the stages
+                                      Context.InputAssembler.SetInputLayout(layout);
+                                      Context.InputAssembler.SetPrimitiveTopology(PrimitiveTopology.TriangleList);
+                                      Context.InputAssembler.SetVertexBuffers(0, new VertexBufferBinding(vertices, 32, 0));
+                                      Context.VertexShader.Set(vertexShader);
+                                      Context.Rasterizer.SetViewports(new Viewport(0, 0, game.Form.ClientSize.Width, game.Form.ClientSize.Height, 0.0f, 1.0f));
+                                      Context.Rasterizer.State = rasterizerState;
+                                      Context.PixelShader.Set(pixelShader);
+                                      Context.OutputMerger.SetTargets(game.RenderView);
+
+                                      Context.Draw(3, 0);
+
+                                  };
+
+            game.Run();
+            /*layout.Release();
+            s.Release();*/
         }
     }
 }
