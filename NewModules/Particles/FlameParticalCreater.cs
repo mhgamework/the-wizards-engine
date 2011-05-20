d871bdb8bbc66fca804f93de33d7ca9c3e4012b0
MHGameWork <mhgamework@gmail.com>
2011-05-20 15:14:26 +0200

    Beware of the FireBall!!
    Signed-off-by: MHGameWork <mhgamework@gmail.com>
    
    Former-commit-id: d550962bbdd0918f7eb9835af88afee9a9e770aa

--- NewModules/Particles/FlameParticalCreater.cs
+++ NewModules/Particles/FlameParticalCreater.cs
@@ -6,20 +6,34 @@ using Microsoft.Xna.Framework;
 
 namespace MHGameWork.TheWizards.Particles
 {
-   public class FlameParticelCreater:IParticleCreater
+    public class FlameParticelCreater : IParticleCreater
     {
         private readonly Emitter emmiter;
         private Seeder seed = new Seeder(123);
 
-       public void GetNewParticleData(out Vector3 position, out Vector3 velocity)
-       {
-           double a = seed.NextFloat(-MathHelper.Pi , MathHelper.Pi);
-           double t = seed.NextFloat(-MathHelper.PiOver2, MathHelper.PiOver2);
-           float rad = seed.NextFloat(0, 1);
-           position = seed.NextVector3(new Vector3(-2, 0, -2), new Vector3(2,0, 2));
-           position = new Vector3((float)Math.Cos(a), 0, (float)Math.Sin(a)) * seed.NextFloat(0, 0.5f);
-           velocity = Vector3.Normalize(new Vector3((float)Math.Cos(a) * rad, rad, (float)Math.Sin(a)) * rad) * 1f;
-       }
-    
+        public void GetNewParticleData(out Vector3 position, out Vector3 velocity)
+        {
+
+            float rad = seed.NextFloat(0, 1);
+            position = randomDirection() * seed.NextFloat(0, 1f);
+            //position = new Vector3((float)(Math.Cos(a) * Math.Cos(t)), (float)Math.Sin(t), (float)(Math.Sin(a) * Math.Cos(t)));
+
+            velocity = randomDirection() * 1f;
+        }
+
+        private Vector3 randomDirection()
+        {
+            //FROM: http://mathworld.wolfram.com/SpherePointPicking.html
+
+            float u = seed.NextFloat(-1, 1);
+            float theta = seed.NextFloat(0, MathHelper.TwoPi);
+
+            float x = (float)(Math.Sqrt(1 - u * u) * Math.Cos(theta));
+            float y = (float)(Math.Sqrt(1 - u * u) * Math.Sin(theta));
+            float z = u;
+
+
+            return new Vector3(x, y, z);
+        }
     }
 }
--- Unit Tests/Particles/ParticleTests.cs
+++ Unit Tests/Particles/ParticleTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using MHGameWork.TheWizards.Graphics;
 using MHGameWork.TheWizards.Particles;
 using MHGameWork.TheWizards.Rendering;
+using MHGameWork.TheWizards.Tests.Graphics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using NUnit.Framework;
@@ -20,7 +21,7 @@ namespace MHGameWork.TheWizards.Tests.Particles
 
             var data = tex.GetCoreData();
             data.StorageType = TextureCoreData.TextureStorageType.Disk;
-            data.DiskFilePath = TWDir.GameData.CreateSubdirectory("Core").FullName+ "\\fire.png";
+            data.DiskFilePath = TWDir.GameData.CreateSubdirectory("Core").FullName + "\\fire.png";
             /*data.StorageType = TextureCoreData.TextureStorageType.Assembly;
             data.Assembly = Assembly.GetExecutingAssembly();
             data.AssemblyResourceName = "MHGameWork.TheWizards.Tests.OBJParser.Files.maps.BrickRound0030_7_S.jpg";*/
@@ -38,8 +39,8 @@ namespace MHGameWork.TheWizards.Tests.Particles
             pool.SetVertexElements<Emitter.ParticleVertex>(Emitter.ParticleVertex.VertexElements);
             var texPool = new TexturePool();
             var testTexture = GetTestTexture();
-            SimpleParticleCreater creater=new SimpleParticleCreater();
-            emit = new Emitter(texPool, pool, game, testTexture,5,5,creater);
+            SimpleParticleCreater creater = new SimpleParticleCreater();
+            emit = new Emitter(texPool, pool, game, testTexture, 5, 5, creater);
             Seeder seed = new Seeder(54);
             game.InitializeEvent += delegate
                                         {
@@ -49,21 +50,21 @@ namespace MHGameWork.TheWizards.Tests.Particles
 
                                             emit.Initialize();
                                             emit.InitializeRender();
-                                            
-                                            
+
+
                                             emit.CreateRenderData();
                                             emit.SetRenderData();
-                                            emit.AddParticles(creater,1);
+                                            emit.AddParticles(creater, 1);
                                         };
             game.UpdateEvent += delegate
                                     {
-                                       // emit.setShader();
+                                        // emit.setShader();
                                         //emit.SetPosition(seed.NextVector3(new Vector3(-50,70,-50),new Vector3(50,70,50)));
                                         emit.TestUpdate();
                                     };
             game.DrawEvent += delegate
                                   {
-                                      game.GraphicsDevice.RenderState.CullMode = CullMode.None;  
+                                      game.GraphicsDevice.RenderState.CullMode = CullMode.None;
                                       emit.Render(game.SpectaterCamera.ViewProjection, game.SpectaterCamera.ViewInverse);
                                   };
 
@@ -82,7 +83,7 @@ namespace MHGameWork.TheWizards.Tests.Particles
             var texPool = new TexturePool();
             var testTexture = GetTestTexture();
             creater = new BallParticleCreater();
-            emit = new Emitter(texPool, pool, game, testTexture, 5f, 5f,creater);
+            emit = new Emitter(texPool, pool, game, testTexture, 5f, 5f, creater);
             Seeder seed = new Seeder(54);
             game.InitializeEvent += delegate
             {
@@ -97,20 +98,20 @@ namespace MHGameWork.TheWizards.Tests.Particles
                 emit.SetRenderData();
                 //emit.AddParticles(creater,1);
             };
-            float  dist = 0;
+            float dist = 0;
             game.UpdateEvent += delegate
             {
                 // emit.setShader();  
                 emit.Update();
-                if (dist >500 )
+                if (dist > 500)
                 {
                     dist = 0;
                 }
                 else
                 {
-                    dist +=game.Elapsed*25;
+                    dist += game.Elapsed * 25;
                 }
-                emit.SetPosition(new Vector3(dist,0,0));
+                emit.SetPosition(new Vector3(dist, 0, 0));
             };
             game.DrawEvent += delegate
             {
@@ -121,7 +122,7 @@ namespace MHGameWork.TheWizards.Tests.Particles
 
             game.Run();
         }
-         [Test]
+        [Test]
         public void FlameTest()
         {
             Emitter emit;
@@ -134,8 +135,11 @@ namespace MHGameWork.TheWizards.Tests.Particles
             var texPool = new TexturePool();
             var testTexture = GetTestTexture();
             creater = new FlameParticelCreater();
-            emit = new Emitter(texPool, pool, game, testTexture, 2f, 2f,creater);
+            emit = new Emitter(texPool, pool, game, testTexture, 2f, 2f, creater);
             Seeder seed = new Seeder(54);
+
+            var curve = Curve3DTester.CreateTestCurve();
+
             game.InitializeEvent += delegate
             {
                 texPool.Initialize(game);
@@ -149,12 +153,14 @@ namespace MHGameWork.TheWizards.Tests.Particles
                 emit.SetRenderData();
                 //emit.AddParticles(creater,1);
             };
-            float  dist = 0;
+            float dist = 0;
             game.UpdateEvent += delegate
             {
+                dist += game.Elapsed;
                 // emit.setShader();  
+                emit.SetPosition(curve.Evaluate(dist*3)*2);
                 emit.Update();
-               
+
             };
             game.DrawEvent += delegate
             {
@@ -165,6 +171,6 @@ namespace MHGameWork.TheWizards.Tests.Particles
 
             game.Run();
         }
-    
+
     }
 }
