1b0b5e70dadfd304d65fc0f135c459267a1658ac
MHGameWork-Laptop <mhgamework@gmail.com>
2011-11-26 18:29:52 +0100

    Work on Model Postsharp Aspect implementation
    Former-commit-id: 0c09b889fcd6828b2dccde55c476754942d33a7e

--- NewModules/Model/Entity.cs
+++ NewModules/Model/Entity.cs
@@ -1,24 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using MHGameWork.TheWizards.Rendering;
+﻿using MHGameWork.TheWizards.Rendering;
 using SlimDX;
 
-namespace MHGameWork.TheWizards.World
+namespace MHGameWork.TheWizards.Model
 {
     /// <summary>
     /// Responsible for representing a independent object in the World. It is not part of any greater system, like the building/terrain/trees
-    /// TODO: moved to model
     /// </summary>
+    [ModelObjectChanged]
     public class Entity
     {
-        private readonly WorldNoSectors world;
-
-        public Entity(WorldNoSectors world)
-        {
-            this.world = world;
-        }
 
         public IMesh Mesh { get; set; }
         public Matrix WorldMatrix { get; set; }
---
+++ NewModules/Model/ModelObjectChangedAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using PostSharp.Aspects;
+using PostSharp.Aspects.Advices;
+using PostSharp.Extensibility;
+using PostSharp.Reflection;
+
+namespace MHGameWork.TheWizards.Model
+{
+    [Serializable]
+    [MulticastAttributeUsage(MulticastTargets.Property)] 
+    public sealed class ModelObjectChangedAttribute :LocationInterceptionAspect
+    {
+        public override void OnSetValue(LocationInterceptionArgs args)
+        {
+            args.ProceedSetValue();
+            if (args.Value != args.GetCurrentValue())
+            {
+                Console.WriteLine("Changeed!");
+            }
+            base.OnSetValue(args);
+        }
+    }
+
+}
