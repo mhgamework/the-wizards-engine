bae32d541a250489bc0533604be3c8fe62b10e69
MHGameWork-Laptop <mhgamework@gmail.com>
2012-04-01 14:54:16 +0200

    Added a Two-way dictionary
    Former-commit-id: 7cedae447f073bf0cff4f7098cd30c51a59a571c

---
+++ Common.Core/Collections/DictionaryTwoWay.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MHGameWork.TheWizards.Collections
+{
+    public class DictionaryTwoWay<T, U>
+    {
+        private Dictionary<T, U> uMap = new Dictionary<T, U>();
+        private Dictionary<U, T> tMap = new Dictionary<U, T>();
+
+        public DictionaryTwoWay()
+        {
+
+        }
+
+        public bool Contains(T t)
+        {
+            return uMap.ContainsKey(t);
+
+        }
+
+        public bool Contains(U u)
+        {
+            return uMap.ContainsKey(u);
+        }
+
+        public void Add(T t, U u)
+        {
+            uMap.Add(t, u);
+            tMap.Add(u, t);
+        }
+
+        public T this[U u] { get { return tMap[u]; } }
+        public U this[T t] { get { return uMap[t]; } }
+    }
+}
