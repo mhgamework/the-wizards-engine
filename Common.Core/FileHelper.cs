22343938d34757e56f9869ddab0eeb3f0ab864dd
MHGameWork-Laptop <mhgamework@gmail.com>
2012-04-04 07:45:10 +0200

    Added FileHelper + Test
    Former-commit-id: b9178a6cfd9db875be22c0bcf83efd8e6b77eeb7

---
+++ Common.Core/FileHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MHGameWork.TheWizards
+{
+    /// <summary>
+    /// Contains helper functions for file-path manipulation
+    /// </summary>
+    public static class FileHelper
+    {
+        /// <summary>
+        /// Returns true when fi is in di or in one of its subdirectories
+        /// </summary>
+        /// <param name="fi"></param>
+        /// <param name="di"></param>
+        /// <returns></returns>
+        public static bool IsFileInDirectory(FileInfo fi, DirectoryInfo di)
+        {
+
+            if (fi.Directory.FullName == di.FullName)
+                return true;
+
+            return IsChildFolder(fi.Directory, di);
+
+
+        }
+
+        /// <summary>
+        /// Returns true when childFolder is a subfolder of parentFolder
+        /// </summary>
+        /// <param name="childFolder"></param>
+        /// <param name="parentFolder"></param>
+        /// <returns></returns>
+        public static bool IsChildFolder(DirectoryInfo childFolder, DirectoryInfo parentFolder)
+        {
+            if (childFolder.Parent == null)
+                return false;
+            childFolder = childFolder.Parent;
+
+            if (childFolder.FullName == parentFolder.FullName)
+                return true;
+
+            return IsChildFolder(childFolder, parentFolder);
+        }
+    }
+}
---
+++ Unit Tests/FileHelperTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Launcher;
+using NUnit.Framework;
+
+namespace MHGameWork.TheWizards.Tests
+{
+    [TestFixture]
+    public class FileHelperTest
+    {
+        private DirectoryInfo rootDir;
+        private DirectoryInfo subDir;
+        private FileInfo rootFile1;
+        private FileInfo subFile1;
+
+        [SetUp]
+        public void SetUp()
+        {
+            rootDir = TWDir.Test.CreateSubdirectory("FileHelperTest");
+            subDir = rootDir.CreateSubdirectory("Sub1");
+            rootFile1 =new FileInfo( rootDir + "\\file1.txt");
+            subFile1 = new FileInfo(subDir + "\\subfile1.txt");
+
+            File.WriteAllText(rootFile1.FullName, "Hello!");
+            File.WriteAllText(subFile1.FullName, "Hello!");
+        }
+
+        [Test]
+        public void TestIsFileInDirectory()
+        {
+            Assert.True(FileHelper.IsFileInDirectory(rootFile1, rootDir));
+            Assert.False(FileHelper.IsFileInDirectory(rootFile1, subDir));
+
+            Assert.True(FileHelper.IsFileInDirectory(subFile1, rootDir));
+            Assert.True(FileHelper.IsFileInDirectory(subFile1, subDir));
+        }
+
+        [Test]
+        public void TestIsChildFolder()
+        {
+            Assert.True(FileHelper.IsChildFolder(subDir, rootDir));
+            Assert.False(FileHelper.IsChildFolder(rootDir, subDir));
+
+            Assert.False(FileHelper.IsChildFolder(rootDir, rootDir));
+            Assert.False(FileHelper.IsChildFolder(subDir, subDir));
+
+        }
+    }
+}
