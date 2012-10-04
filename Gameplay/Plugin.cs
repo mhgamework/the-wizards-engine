46b204c0a451674d79205cf99a67252bb4e9d498
Simon Chuptys <simon.chuptys@gmail.com>
2012-10-04 15:35:21 +0200

    LevelBuilding: Made player invisible
    Former-commit-id: 808ef1a303db4671c5b19ecda5e4472e46fc811a

--- Gameplay/Plugin.cs
+++ Gameplay/Plugin.cs
@@ -293,6 +293,7 @@ namespace MHGameWork.TheWizards
         private void testLevelBuilding(TWEngine engine)
         {
             var player = new PlayerData();
+            player.Entity.Visible = false;
             var cameraInfo = TW.Data.GetSingleton<CameraInfo>();
             cameraInfo.Mode = CameraInfo.CameraMode.ThirdPerson;
             cameraInfo.FirstPersonCameraTarget = player.Entity;
