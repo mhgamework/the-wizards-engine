1925a13be9328b8effc251ad4bbdb39e2c1ef551
MHGW-Laptop <mhgamework@gmail.com>
2012-10-26 19:46:42 +0200

    Work on texture loading
    Former-commit-id: ae903695eeef2b9ab6a388f686e31322d3a9c85f

---
+++ Gameplay/CG/ITexture2DLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MHGameWork.TheWizards.Rendering;
+
+namespace MHGameWork.TheWizards.CG
+{
+    /// <summary>
+    /// Responsible for creating/caching CG.Texture2D's from ITexture objects
+    /// </summary>
+    public interface ITexture2DLoader
+    {
+        Texture2D Load(ITexture texture);
+    }
+}
---
+++ Gameplay/CG/SimpleTexture2DLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using MHGameWork.TheWizards.Rendering;
+using SlimDX;
+
+namespace MHGameWork.TheWizards.CG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SimpleTexture2DLoader : ITexture2DLoader
+    {
+        private Dictionary<ITexture, Texture2D> cache = new Dictionary<ITexture, Texture2D>();
+        public Texture2D Load(ITexture texture)
+        {
+            if (cache.ContainsKey(texture)) return cache[texture];
+
+            var bitmap = new BitmapImage(new Uri(texture.GetCoreData().DiskFilePath));
+            int offset;
+            
+            int bytesPerPixel = (bitmap.Format.BitsPerPixel / 8);
+            int stride = bitmap.PixelWidth * bytesPerPixel;
+            Color4[] pixels = new Color4[bitmap.PixelWidth * bitmap.PixelHeight * bytesPerPixel];
+            bitmap.CopyPixels(pixels, stride, 0);
+
+
+        }
+    }
+}
