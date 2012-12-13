d807bce2f3fc921db0fed033ee904086f20ee2a8
MHGameWorkBase <mhgamework@gmail.com>
2012-12-14 00:05:13 +0100

    Added crazyvoxels algorithm source
    Former-commit-id: b1621d191a0da18987a78ef9e041a52adfba1460

---
+++ _Research/Crazyvoxels/isosurf/threed/opengl.h
@@ -0,0 +1,22 @@
+#include <w32api/GL/gl.h>
+#include <w32api/GL/glu.h>
+/*
+#ifdef __CYGWIN32__
+#define APIENTRY
+#define CALLBACK
+#define WINGDIAPI __attribute__ ((__stdcall__))
+#endif
+#include <GL/glut.h>
+*/
+/*
+#ifdef _MSC_VER
+#include <windows.h>
+#undef APIENTRY
+#undef WINGDIAPI
+#define APIENTRY __cdecl
+#define WINGDIAPI
+#pragma comment (lib, "opengl32.lib")  /* link with Microsoft OpenGL lib */
+//#pragma comment (lib, "glu32.lib")     /* link with Microsoft OpenGL Utility lib */
+//#endif
+
+//#include <GL/gl.h>
