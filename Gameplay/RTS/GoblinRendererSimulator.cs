6815f46e9e663e797e411f1faa1d13e7ffcfa237
Jasper Hilven <jasperhilven@hotmail.com>
2012-12-15 19:36:41 +0100

    fixed test wrong location
    Signed-off-by: Jasper Hilven <jasperhilven@hotmail.com>
    
    Former-commit-id: d546e5df825b332618861cb32a478dd583cba13b

---
+++ Gameplay/RTS/GoblinRendererSimulator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MHGameWork.TheWizards.Engine;
+using MHGameWork.TheWizards.MathExtra;
+using MHGameWork.TheWizards.WorldRendering;
+using SlimDX;
+
+namespace MHGameWork.TheWizards.RTS
+{
+    class GoblinRendererSimulator: ISimulator
+    {
+        public void Simulate()
+        {
+            foreach (var goblin in TW.Data.GetChangedObjects<Goblin>())
+            {
+                if (goblin.get<GoblinRenderData>() == null)
+                    goblin.set(new GoblinRenderData { LookDirection = new Vector3(0, 0, 1) });
+                var ent = goblin.get<WorldRendering.Entity>();
+                fixRendering(goblin, ent);
+            }
+
+
+        }
+        private static void fixRendering(Goblin goblin, WorldRendering.Entity ent)
+        {
+            var renderData = goblin.get<GoblinRenderData>();
+            if ((renderData.LastPosition - goblin.Position).Length() > 0.001f)
+            {
+                renderData.LookDirection = goblin.Position - renderData.LastPosition;
+            }
+            renderData.LastPosition = goblin.Position;
+            var quat = Functions.CreateFromLookDir(-Vector3.Normalize(renderData.LookDirection).xna());
+            ent.WorldMatrix = Microsoft.Xna.Framework.Matrix.CreateFromQuaternion(quat).dx() * Matrix.Scaling(0.01f, 0.01f, 0.01f) *
+                              Matrix.Translation(goblin.Position);
+
+            renderData.LastPosition = goblin.Position;
+            ent.Mesh = MeshFactory.Load("Goblin\\GoblinLowRes");
+            ent.Solid = true;
+            ent.Static = false;
+        }
+        private class GoblinRenderData
+        {
+            public Vector3 LastPosition { get; set; }
+            public Vector3 LookDirection { get; set; }
+        }
+    }
+}
