aabb1fa04b4fac130bf686da0db70127cb08a3fe
Jasper Hilven <jasperhilven@hotmail.com>
2012-12-21 13:30:58 +0100

    commit for popping stash
    Signed-off-by: Jasper Hilven <jasperhilven@hotmail.com>
    
    Former-commit-id: 6fb0bb105c938f6aa537553531afa1316262c34e

---
+++ Gameplay/RTS/GoblinCommanderSimulator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MHGameWork.TheWizards.Engine;
+using MHGameWork.TheWizards.Raycasting;
+using MHGameWork.TheWizards.VoxelTerraining;
+using MHGameWork.TheWizards.WorldRendering;
+using SlimDX.DirectInput;
+using MHGameWork.TheWizards;
+using System.Drawing;
+
+namespace MHGameWork.TheWizards.RTS
+{
+    /// <summary>
+    /// Press T to select goblin, Y to move goblin to a position.
+    /// 
+    /// </summary>
+    public class GoblinCommanderSimulator : ISimulator
+    {
+        private Goblin currentSelectedGoblin;
+        public void Simulate()
+        {
+            var terrain = TW.Data.GetSingleton<VoxelTerrain>();
+            var world = TW.Data.GetSingleton<WorldRendering.World>();
+            if (TW.Graphics.Keyboard.IsKeyPressed(Key.T))
+            {
+                selectGoblin(world);
+            }
+            if (TW.Graphics.Keyboard.IsKeyPressed(Key.Y))
+            {
+                moveGoblin(terrain);
+            }
+         if (TW.Graphics.Keyboard.IsKeyPressed(Key.U))
+         {
+             if (currentSelectedGoblin == null)
+                 return;
+             VoxelBlock last;
+             terrain.Raycast(TW.Data.GetSingleton<CameraInfo>().GetCenterScreenRay(), out last);
+             if (last == null)
+                 return;
+             var start = terrain.GetPositionOf(last);
+             currentSelectedGoblin.Position = start;
+
+         }
+        }
+
+        private void moveGoblin(VoxelTerrain terrain)
+        {
+            VoxelBlock last;
+            terrain.Raycast(TW.Data.GetSingleton<CameraInfo>().GetCenterScreenRay(), out last);
+            if (last == null)
+                return;
+            var start = terrain.GetPositionOf(last);
+            if (currentSelectedGoblin == null)
+                return;
+            if (currentSelectedGoblin.get<GoblinMover>() == null)
+                currentSelectedGoblin.set(new GoblinMover(currentSelectedGoblin));
+            var goblinMover = currentSelectedGoblin.get<GoblinMover>();
+            goblinMover.MoveTo(start);
+        }
+
+        private void selectGoblin(WorldRendering.World world)
+        {
+            var ray = TW.Data.GetSingleton<CameraInfo>().GetCenterScreenRay();
+            var result = world.Raycast(ray);
+            if (!result.IsHit)
+                return;
+            if (!(result.Object is Goblin))
+                return;
+                currentSelectedGoblin = (Goblin) result.Object;
+        
+        
+        
+        
+        }
+    }
+}
---
+++ Unit Tests/Gameplay/RTS/TestGoblinWalking.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+using DirectX11;
+using MHGameWork.TheWizards.Engine;
+using MHGameWork.TheWizards.RTS;
+using MHGameWork.TheWizards.Simulators;
+using MHGameWork.TheWizards.Tests.Gameplay.Core;
+using MHGameWork.TheWizards.VoxelTerraining;
+using MHGameWork.TheWizards.WorldRendering;
+using NUnit.Framework;
+using SlimDX;
+
+namespace MHGameWork.TheWizards.Tests.Gameplay.RTS
+{
+    [TestFixture]
+    public class TestGoblinWalking
+    {
+        [Test]
+        public void TestRenderWalking()
+        {
+            var engine = new TWEngine { DontLoadPlugin = true };
+            engine.Initialize();
+            VoxelTerrainTest.generateTerrain(1, 1);
+            var world = TW.Data.GetSingleton<WorldRendering.World>();
+            var TestGoblin = new Goblin();
+            engine.AddSimulator(new VoxelTerrainSimulator());
+            engine.AddSimulator(new GoblinCommanderSimulator());
+            engine.AddSimulator(new GoblinRendererSimulator());
+            engine.AddSimulator(new WorldRenderingSimulator());
+            engine.Run();
+        }
+        private static VoxelTerrainChunk createTerrain()
+        {
+            var terr = new VoxelTerrainChunk();
+            terr.Size = new Point3(10, 10, 10);
+            terr.Create();
+
+            var random = new Random();
+            for (int i = 0; i < 30; i++)
+            {
+                terr.GetVoxelInternal(new Point3(random.Next(9), random.Next(9), random.Next(9))).Filled = true;
+            }
+
+            terr.GetVoxelInternal(new Point3(1, 1, 1)).Filled = true;
+            return terr;
+        }
+
+    }
+}
