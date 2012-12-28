35376643cd8669302452aed3adcb42b660b5946c
MHGW-Laptop <mhgamework@gmail.com>
2012-12-28 22:14:11 +0100

    Created autobuild batch script
    Former-commit-id: d525ec2c042de9f829a791a772f0ac1be45cce45

---
+++ BuildAll.bat
@@ -0,0 +1,4 @@
+cd _scripts
+call clean.bat
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "The Wizards.sln" 
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "The Wizards.sln" 
