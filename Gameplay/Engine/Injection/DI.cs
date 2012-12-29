a63994f96c74adb8c73841d40686748f50eb6379
MHGW-Laptop <mhgamework@gmail.com>
2012-12-29 18:50:09 +0100

    Added simple DI
    Former-commit-id: a460325e51967dba49739d2f4bc26f52078baa0d

---
+++ Gameplay/Engine/Injection/DI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MHGameWork.TheWizards.Engine.Injection
+{
+    /// <summary>
+    /// Responsible for resolving dependencies!
+    /// Maybe merge into TW??
+    /// </summary>
+    public static class DI
+    {
+        private static Dictionary<Type, object> items = new Dictionary<Type, object>();
+        public static T Get<T>()
+        {
+            object ret = null;
+            if (!items.ContainsKey(typeof (T)))
+            {
+                ret = Activator.CreateInstance(typeof (T));
+                items.Add(typeof(T),ret);
+            }
+            else
+            {
+                ret = items[typeof (T)];
+            }
+
+            return (T) ret;
+        }
+    }
+}
