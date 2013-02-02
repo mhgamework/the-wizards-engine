0162a6622cdcb3b95638bcea71ca2a2722639b11
MHGW-Laptop <mhgamework@gmail.com>
2013-02-02 20:25:58 +0100

    Upgraded BuildAll.bat, now also gameplay
    Former-commit-id: 7273cb8a6ce8c5c5dd023bd657f00f2b23d25c66

--- BuildAll.bat
+++ BuildAll.bat
@@ -2,4 +2,5 @@ cd _scripts
 call clean.bat
 "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "The Wizards.sln" 
 "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "The Wizards.sln" 
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "The Wizards Gameplay\The Wizards Gameplay.sln" 
 xcopy /s "The Wizards Gameplay\packages\QuickGraph.3.6.61119.7\lib\net4" "bin\Binaries"
