c66c9e20ccdc2c0522323a7792c048b2064329f9
MHGW-Laptop <mhgamework@gmail.com>
2013-03-13 10:34:54 +0100

    Work on FieldDensityCalculator.cs
    Former-commit-id: aa773d461135864f00d7e13d7d8f0c9a978d4ff9

--- Gameplay/RTSTestCase1/Magic/FieldDensityCalculator.cs
+++ Gameplay/RTSTestCase1/Magic/FieldDensityCalculator.cs
@@ -1,7 +1,24 @@
-﻿namespace MHGameWork.TheWizards.RTSTestCase1.Magic
+﻿using System.Collections.Generic;
+
+namespace MHGameWork.TheWizards.RTSTestCase1.Magic
 {
+    /// <summary>
+    /// Responsible for calculating the energy densities, given a grid to insert the approximate densities into
+    /// </summary>
     public class FieldDensityCalculator
     {
-         
+         public void CalculateDensities(List<IFieldElement> elements, IGrid grid)
+         {
+             
+         }
+
+    }
+
+    public interface IGrid
+    {
+    }
+
+    public interface IFieldElement
+    {
     }
 }
---
+++ Gameplay/RTSTestCase1/_Tests/MagicTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using MHGameWork.TheWizards.Engine;
+using MHGameWork.TheWizards.Gameplay;
+using MHGameWork.TheWizards.RTSTestCase1.Magic;
+using NUnit.Framework;
+using SlimDX;
+
+namespace MHGameWork.TheWizards.RTSTestCase1._Tests
+{
+    [TestFixture]
+    public class MagicTest
+    {
+        private TWEngine engine = EngineFactory.CreateEngine();
+
+        [Test]
+        public void TestFieldDensityCalculator()
+        {
+            var calc = new FieldDensityCalculator();
+            var gridResolution = 10;
+            var grid = createGrid(gridResolution);
+
+            var list = new List<IFieldElement>();
+
+            list.Add(createElement(new Vector3(5, 0, 5), 10));
+            list.Add(createElement(new Vector3(5, 0, 5), 10));
+
+            list.Add(createElement(new Vector3(-5, 0, 5), 10));
+            list.Add(createElement(new Vector3(-5, 0, -5), 10));
+
+            calc.CalculateDensities(list,grid);
+
+            // TODO: test 1 cell with 20, 2 cells with 10 and all the rest 0
+
+        }
+
+        private IFieldElement createElement(Vector3 p0, int p1)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private IGrid createGrid(int gridResolution)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
