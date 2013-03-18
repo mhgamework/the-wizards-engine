a157db74248c4b6c01684e9e141390413fc6f10c
MHGW-Laptop <mhgamework@gmail.com>
2013-03-18 19:09:51 +0100

    Added console output to the TestRunnerGUI.cs
    Former-commit-id: bbefc3a3a4573edda0db9a04cc194c7d6f47473a

---
+++ TestRunner/ConsoleOutputTestRunner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MHGameWork.TheWizards.TestRunner
+{
+    /// <summary>
+    /// Test runner decorater which adds console info when starting and completing tests
+    /// Only decorates the NUnitTestrunner
+    /// </summary>
+    public class ConsoleOutputTestRunner : ITestRunner
+    {
+        private readonly NUnitTestRunner runner;
+
+        public ConsoleOutputTestRunner(NUnitTestRunner runner)
+        {
+            this.runner = runner;
+        }
+
+        public void Run(ITest test)
+        {
+            var n = (NUnitTest)test;
+
+            var name = n.TestClass.FullName + " - " + n.TestMethod.Name;
+
+            Console.WriteLine("Starting test " + name);
+            runner.Run(test);
+            Console.WriteLine("Completed test " + name);
+
+        }
+    }
+}
