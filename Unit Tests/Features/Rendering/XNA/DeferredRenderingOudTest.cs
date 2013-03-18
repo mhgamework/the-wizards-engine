80321a6b7db86027c8fb2dcc41a9a182647ad1c9
MHGW-Laptop <mhgamework@gmail.com>
2013-03-18 17:09:08 +0100

    Cleaned up old XNA rendering tests
    Former-commit-id: 625ef6ba4e719f8c1c678457eaf34efd66d36a88

---
+++ Unit Tests/Features/Rendering/XNA/DeferredRenderingOudTest.cs
@@ -0,0 +1,73 @@
+﻿using MHGameWork.TheWizards.Graphics;
+using MHGameWork.TheWizards.Rendering.Deferred;
+using NUnit.Framework;
+
+namespace MHGameWork.TheWizards.Tests.Features.Rendering
+{
+    [TestFixture]
+    public class DeferredRenderingOudTest
+    {
+        [Test]
+        [RequiresThread(System.Threading.ApartmentState.STA)]
+        public void TestDeferredRenderer()
+        {
+            var game = new XNAGame();
+
+            var renderer = new DeferredRendererOud(RenderingTest.CreateMerchantsHouseMeshOLD(), TestFiles.WoodPlanksBareJPG);
+
+            renderer.OutputMode = DeferredRendererOud.DeferredOutputMode.Diffuse;
+
+            game.AddXNAObject(renderer);
+
+            game.UpdateEvent += delegate
+                                    {
+                                        if (game.Keyboard.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.D1))
+                                        {
+                                            renderer.OutputMode = DeferredRendererOud.DeferredOutputMode.Final;
+                                        }
+                                        if (game.Keyboard.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.D2))
+                                        {
+                                            renderer.OutputMode = DeferredRendererOud.DeferredOutputMode.Diffuse;
+                                        }
+                                        if (game.Keyboard.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.D3))
+                                        {
+                                            renderer.OutputMode = DeferredRendererOud.DeferredOutputMode.Normal;
+                                        }
+                                        if (game.Keyboard.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.D4))
+                                        {
+                                            renderer.OutputMode = DeferredRendererOud.DeferredOutputMode.Depth;
+                                        }
+                                        if (game.Keyboard.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.D5))
+                                        {
+                                            renderer.OutputMode = DeferredRendererOud.DeferredOutputMode.LightAccumulation;
+                                        }
+                                        if (game.Keyboard.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.D6))
+                                        {
+                                            renderer.OutputMode = DeferredRendererOud.DeferredOutputMode.ShadowMap;
+                                        }
+                                        if (game.Keyboard.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.D7))
+                                        {
+                                            renderer.OutputMode = DeferredRendererOud.DeferredOutputMode.AmbientOcclusion;
+                                        }
+                                        if (game.Keyboard.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.D8))
+                                        {
+                                            renderer.OutputMode = DeferredRendererOud.DeferredOutputMode.BlurX;
+                                        }
+                                        if (game.Keyboard.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.D9))
+                                        {
+                                            renderer.OutputMode = DeferredRendererOud.DeferredOutputMode.BlurY;
+                                        }
+                                        if (game.Keyboard.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.NumPad1))
+                                            renderer.OutputMode = DeferredRendererOud.DeferredOutputMode.Downsample;
+                                        if (game.Keyboard.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.NumPad2))
+                                            renderer.OutputMode = DeferredRendererOud.DeferredOutputMode.DownsampleBlurX;
+                                        if (game.Keyboard.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.NumPad3))
+                                            renderer.OutputMode = DeferredRendererOud.DeferredOutputMode.DownsampleBlurY;
+                                        if (game.Keyboard.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.NumPad4))
+                                            renderer.OutputMode = DeferredRendererOud.DeferredOutputMode.ToneMapped;
+                                    };
+
+            game.Run();
+        }
+    }
+}
---
+++ Unit Tests/Features/Rendering/XNA/Terrain/SimpleTerrainBlock.cs
@@ -0,0 +1,33 @@
+﻿using MHGameWork.TheWizards.Common.GeoMipMap;
+using Microsoft.Xna.Framework.Graphics;
+using ITerrainBlock = MHGameWork.TheWizards.Terrain.Geomipmap.ITerrainBlock;
+
+namespace MHGameWork.TheWizards.Tests.Features.Rendering.Terrain
+{
+    public class SimpleTerrainBlock : ITerrainBlock
+    {
+        public IndexBuffer IndexBuffer { get; set; }
+        public int DetailLevel { get; set; }
+
+        public int TriangleCount { get; set; }
+
+        public float[] MinDistancesSquared { get; set; }
+
+        private ITerrainBlock[] neighbours = new ITerrainBlock[4];
+
+        public ITerrainBlock GetNeighbour(TerrainBlockEdge edge)
+        {
+            return neighbours[(int)edge];
+        }
+        public void SetNeightbour(TerrainBlockEdge edge, ITerrainBlock value)
+        {
+            neighbours[(int)edge] = value;
+        }
+
+        public SimpleTerrainBlock()
+        {
+            DetailLevel = -1;
+        }
+
+    }
+}
