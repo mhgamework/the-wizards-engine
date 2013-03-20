2dca440e9234963ab5252400411f298fd6a14b62
MHGameWorkBase <mhgamework@gmail.com>
2013-03-20 15:33:02 +0100

    Moved around some tests for rendering
    Conflicts:
    	Unit Tests/Features/Rendering/DeferredRenderingTest.cs
    
    Former-commit-id: fe36b12d4037d80984b7a4282644a0a749a5a6b4

---
+++ Unit Tests/Features/Rendering/Deferred/DeferredMeshRendererTest.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.IO;
+using DirectX11;
+using MHGameWork.TheWizards.DirectX11;
+using MHGameWork.TheWizards.DirectX11.Graphics;
+using MHGameWork.TheWizards.DirectX11.Rendering.Deferred;
+using MHGameWork.TheWizards.OBJParser;
+using MHGameWork.TheWizards.Rendering;
+using MHGameWork.TheWizards.Rendering.Deferred;
+using MHGameWork.TheWizards.Tests.Features.Rendering.DirectX11;
+using NUnit.Framework;
+using SlimDX;
+using SlimDX.DirectInput;
+
+namespace MHGameWork.TheWizards.Tests.Features.Rendering.Deferred
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestFixture]
+    public class DeferredMeshRendererTest
+    {
+        [Test]
+        public void TestMeshRendererSimpleCulling()
+        {
+            var game = new DX11Game();
+            game.InitDirectX();
+            var context = game.Device.ImmediateContext;
+
+            var mesh = RenderingTestsHelper.CreateSimpleTestMesh();
+
+            var texturePool = new TheWizards.Rendering.Deferred.TexturePool(game);
+
+            var gBuffer = new GBuffer(game.Device, 800, 600);
+
+            var renderer = new DeferredMeshRenderer(game, gBuffer, texturePool);
+
+
+            DeferredMeshRenderElement middle = null;
+
+
+
+
+
+
+
+
+
+            Vector3 radius = new Vector3(100, 1000, 100);
+            FrustumCullerSimple culler = new FrustumCullerSimple(new BoundingBox(-radius, radius), 5);
+
+            //QuadTreeVisualizer visualizer = new QuadTreeVisualizer();
+
+
+            SpectaterCamera cullCam = new SpectaterCamera(game.Keyboard, game.Mouse, 10f, 80);
+
+            cullCam.Positie = new Vector3(8, 10, 8);
+            cullCam.EnableUserInput = false;
+
+            SpectaterCamera renderCam = game.SpectaterCamera;
+
+            culler.CullCamera = cullCam;
+
+            throw new NotImplementedException();
+            //renderer.Culler = culler;
+
+            bool rotate = true;
+            int selectedNode = -1;
+
+
+
+            for (int i = 0; i < 50; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+
+                    var el = renderer.AddMesh(mesh);
+                    el.WorldMatrix = Matrix.Translation(MathHelper.Right * i * 2 + Vector3.UnitZ * j * 2);
+
+                    if (i > 20 && i < 30 && j > 20 && j < 30)
+                        el.Delete();
+                }
+
+            }
+            game.GameLoopEvent += delegate
+            {
+                culler.UpdateVisibility();
+
+                if (rotate)
+                    cullCam.AngleHorizontal += game.Elapsed * MathHelper.Pi * (1 / 8f);
+                cullCam.Update(game.Elapsed);
+                if (game.Keyboard.IsKeyPressed(Key.NumberPadPlus))
+                    selectedNode++;
+                if (game.Keyboard.IsKeyPressed(Key.NumberPadMinus))
+                    selectedNode--;
+
+                if (game.Keyboard.IsKeyPressed(Key.Return))
+                {
+                    int count = -1;
+                    //visualizer.RenderNodeGroundBoundig(game, culler.RootNode,
+                    //delegate(FrustumCuller.CullNode node, out Color col)
+                    //{
+                    //    col = Color.Red;
+                    //    count++;
+                    //    if (count == selectedNode)
+                    //    {
+                    //        node.Tag = "SELECTED!";
+                    //    }
+                    //    return count == selectedNode;
+                    //});
+                }
+
+                if (game.Keyboard.IsKeyPressed(Key.NumberPad0))
+                    rotate = !rotate;
+
+
+                game.LineManager3D.AddViewFrustum(new BoundingFrustum(cullCam.ViewProjection), new Color4());
+                //for (int i = 0; i < cullObjects.Count; i++)
+                //{
+                //    game.LineManager3D.AddBox(cullObjects[i].BoundingBox, Color.Red);
+                //}
+                //visualizer.RenderNodeGroundBoundig(game, culler.RootNode,
+                //    delegate(FrustumCuller.CullNode node, out Color col)
+                //    {
+                //        if (node.Visible)
+                //        {
+                //            col = Color.Orange;
+                //        }
+                //        else
+                //        {
+                //            col = Color.Green;
+
+                //        }
+
+                //        return true;
+                //    });
+
+
+
+
+
+
+
+
+                gBuffer.Clear();
+                gBuffer.SetTargetsToOutputMerger();
+
+                context.Rasterizer.State = game.HelperStates.RasterizerShowAll;
+
+                renderer.Draw();
+
+                context.ClearState();
+                game.SetBackbuffer();
+
+                DeferredTest.DrawGBuffer(game, gBuffer);
+            };
+
+            game.Run();
+
+        }
+
+        [Test]
+        public void TestMeshRendererAdvanced()
+        {
+            var texFactory = new RAMTextureFactory();
+            var c = new OBJToRAMMeshConverter(texFactory);
+
+            var game = new DX11Game();
+            game.InitDirectX();
+            var context = game.Device.ImmediateContext;
+
+            var importer = new ObjImporter();
+            importer.AddMaterialFileStream("Crate01.mtl", File.OpenRead(TestFiles.CrateMtl));
+            importer.ImportObjFile(TestFiles.CrateObj);
+
+            var mesh = c.CreateMesh(importer);
+
+            RAMMesh mesh2 = RenderingTestsHelper.CreateMerchantsHouseMesh(c);
+
+
+            RAMMesh mesh3 = RenderingTestsHelper.CreateGuildHouseMesh(c);
+
+            var gBuffer = new GBuffer(game.Device, 800, 600);
+
+            DeferredMeshRenderer renderer = InitDefaultMeshRenderer(game, gBuffer);
+
+
+            var el = renderer.AddMesh(mesh);
+            el.WorldMatrix = Matrix.Translation(MathHelper.Right * 0 * 2 + Vector3.UnitZ * 0 * 2);
+
+            el = renderer.AddMesh(mesh2);
+            el.WorldMatrix = Matrix.Translation(new Vector3(0, 0, 80));
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    el = renderer.AddMesh(mesh3);
+                    el.WorldMatrix = Matrix.Translation(new Vector3(j * 30, 0, 70 + i * 30));
+                }
+            }
+
+
+
+            game.GameLoopEvent += delegate
+            {
+                gBuffer.Clear();
+                gBuffer.SetTargetsToOutputMerger();
+
+                context.Rasterizer.State = game.HelperStates.RasterizerShowAll;
+
+                renderer.Draw();
+
+                context.ClearState();
+                game.SetBackbuffer();
+
+                DeferredTest.DrawGBuffer(game, gBuffer);
+            };
+            game.Run();
+
+        }
+
+
+
+
+
+        [Test]
+        public void TestMeshRendererSimple()
+        {
+            var game = new DX11Game();
+            game.InitDirectX();
+            var context = game.Device.ImmediateContext;
+
+            var mesh = RenderingTestsHelper.CreateSimpleTestMesh();
+
+            var texturePool = new TheWizards.Rendering.Deferred.TexturePool(game);
+
+            var gBuffer = new GBuffer(game.Device, 800, 600);
+
+            var renderer = new DeferredMeshRenderer(game, gBuffer, texturePool);
+
+
+            DeferredMeshRenderElement middle = null;
+
+            for (int i = 0; i < 50; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+
+                    var el = renderer.AddMesh(mesh);
+                    el.WorldMatrix = Matrix.Translation(MathHelper.Right * i * 2 + Vector3.UnitZ * j * 2);
+
+                    if (i > 20 && i < 30 && j > 20 && j < 30)
+                        el.Delete();
+                }
+
+            }
+
+            game.GameLoopEvent += delegate
+            {
+                gBuffer.Clear();
+                gBuffer.SetTargetsToOutputMerger();
+
+                context.Rasterizer.State = game.HelperStates.RasterizerShowAll;
+
+                renderer.Draw();
+
+                context.ClearState();
+                game.SetBackbuffer();
+
+                DeferredTest.DrawGBuffer(game, gBuffer);
+            };
+
+
+            game.Run();
+
+        }
+
+
+
+
+
+
+
+
+        public static DeferredMeshRenderer InitDefaultMeshRenderer(DX11Game game, GBuffer gBuffer)
+        {
+            var texturePool = new TheWizards.Rendering.Deferred.TexturePool(game);
+
+            var renderer = new DeferredMeshRenderer(game, gBuffer, texturePool);
+
+            return renderer;
+        }
+
+    }
+}
---
+++ Unit Tests/Features/Rendering/Deferred/DeferredRendererTest.cs
@@ -0,0 +1,203 @@
+﻿using DirectX11;
+using MHGameWork.TheWizards.DirectX11;
+using MHGameWork.TheWizards.DirectX11.Graphics;
+using MHGameWork.TheWizards.OBJParser;
+using MHGameWork.TheWizards.Rendering;
+using MHGameWork.TheWizards.Rendering.Deferred;
+using NUnit.Framework;
+using SlimDX;
+using SlimDX.DirectInput;
+
+namespace MHGameWork.TheWizards.Tests.Features.Rendering.Deferred
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestFixture]
+    public class DeferredRendererTest
+    {
+        [Test]
+        public void TestDeferredRendererLineElement()
+        {
+            var game = new DX11Game();
+            game.InitDirectX();
+
+            var renderer = new DeferredRenderer(game);
+
+            var el = renderer.CreateLinesElement();
+
+            el.Lines.AddBox(new BoundingBox(Vector3.Zero, MathHelper.One), new Color4(1, 0, 0));
+
+
+            game.GameLoopEvent += delegate
+            {
+                renderer.Draw();
+
+            };
+
+            game.Run();
+        }
+
+
+        [Test]
+        public void TestDeferredRendererSimple()
+        {
+            var game = new DX11Game();
+            game.InitDirectX();
+
+            var renderer = new DeferredRenderer(game);
+
+            var otherCam = new SpectaterCamera(game.Keyboard, game.Mouse, 1, 10000);
+
+            var mesh = RenderingTestsHelper.CreateMerchantsHouseMesh(new OBJToRAMMeshConverter(new RAMTextureFactory()));
+
+            var el = renderer.CreateMeshElement(mesh);
+            var directional = renderer.CreateDirectionalLight();
+            directional.ShadowsEnabled = true;
+            var point = renderer.CreatePointLight();
+            point.LightRadius *= 2;
+            point.ShadowsEnabled = true;
+            var spot = renderer.CreateSpotLight();
+            spot.LightRadius *= 2;
+            spot.ShadowsEnabled = true;
+
+            int state = 0;
+
+            var camState = false;
+
+            game.GameLoopEvent += delegate
+            {
+                if (game.Keyboard.IsKeyPressed(Key.D1))
+                    state = 0;
+                if (game.Keyboard.IsKeyPressed(Key.D2))
+                    state = 1;
+                if (game.Keyboard.IsKeyPressed(Key.D3))
+                    state = 2;
+                if (game.Keyboard.IsKeyPressed(Key.D4))
+                    state = 3;
+
+                switch (state)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        directional.LightDirection = game.SpectaterCamera.CameraDirection;
+                        break;
+                    case 2:
+                        point.LightPosition = game.SpectaterCamera.CameraPosition;
+
+                        break;
+                    case 3:
+                        spot.LightPosition = game.SpectaterCamera.CameraPosition;
+                        spot.SpotDirection = game.SpectaterCamera.CameraDirection;
+                        break;
+                }
+
+                if (game.Keyboard.IsKeyPressed(Key.C))
+                    camState = !camState;
+
+                if (camState)
+                {
+                    game.Camera = game.SpectaterCamera;
+                    renderer.DEBUG_SeperateCullCamera = null;
+                }
+                else
+                {
+                    game.Camera = otherCam;
+                    renderer.DEBUG_SeperateCullCamera = game.SpectaterCamera;
+
+
+                }
+                game.SpectaterCamera.EnableUserInput = camState;
+                otherCam.EnableUserInput = !camState;
+                otherCam.Update(game.Elapsed);
+                renderer.Draw();
+
+            };
+
+            game.Run();
+        }
+        [Test]
+        public void TestDeferredRendererCastsShadowsField()
+        {
+            var game = new DX11Game();
+            game.InitDirectX();
+
+            var renderer = new DeferredRenderer(game);
+
+            var otherCam = new SpectaterCamera(game.Keyboard, game.Mouse, 1, 10000);
+
+            var mesh = RenderingTestsHelper.CreateMerchantsHouseMesh(new OBJToRAMMeshConverter(new RAMTextureFactory()));
+
+
+            var el = renderer.CreateMeshElement(mesh);
+            el.CastsShadows = false;
+            var directional = renderer.CreateDirectionalLight();
+            directional.ShadowsEnabled = true;
+            var point = renderer.CreatePointLight();
+            point.LightRadius *= 2;
+            point.ShadowsEnabled = true;
+            var spot = renderer.CreateSpotLight();
+            spot.LightRadius *= 2;
+            spot.ShadowsEnabled = true;
+
+            int state = 0;
+
+            var camState = false;
+
+            game.GameLoopEvent += delegate
+            {
+                if (game.Keyboard.IsKeyPressed(Key.D1))
+                    state = 0;
+                if (game.Keyboard.IsKeyPressed(Key.D2))
+                    state = 1;
+                if (game.Keyboard.IsKeyPressed(Key.D3))
+                    state = 2;
+                if (game.Keyboard.IsKeyPressed(Key.D4))
+                    state = 3;
+
+                switch (state)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        directional.LightDirection = game.SpectaterCamera.CameraDirection;
+                        break;
+                    case 2:
+                        point.LightPosition = game.SpectaterCamera.CameraPosition;
+
+                        break;
+                    case 3:
+                        spot.LightPosition = game.SpectaterCamera.CameraPosition;
+                        spot.SpotDirection = game.SpectaterCamera.CameraDirection;
+                        break;
+                }
+
+                if (game.Keyboard.IsKeyPressed(Key.C))
+                    camState = !camState;
+
+                if (camState)
+                {
+                    game.Camera = game.SpectaterCamera;
+                    renderer.DEBUG_SeperateCullCamera = null;
+                }
+                else
+                {
+                    game.Camera = otherCam;
+                    renderer.DEBUG_SeperateCullCamera = game.SpectaterCamera;
+
+
+                }
+                game.SpectaterCamera.EnableUserInput = camState;
+                otherCam.EnableUserInput = !camState;
+                otherCam.Update(game.Elapsed);
+                renderer.Draw();
+
+            };
+
+            game.Run();
+        }
+
+
+    }
+}
