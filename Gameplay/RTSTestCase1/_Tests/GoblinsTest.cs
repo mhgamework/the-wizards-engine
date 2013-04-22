517739f7cb1e11050d6169837c3324f73c97a270
MHGW-Laptop <mhgamework@gmail.com>
2013-04-22 20:47:57 +0200

    Started work on goblin commands
    Former-commit-id: 7af172ec3a14c9333ea583f8a821e5d5cb40091a

---
+++ Gameplay/RTSTestCase1/_Tests/GoblinsTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MHGameWork.TheWizards.Engine;
+using MHGameWork.TheWizards.Engine.Features.Testing;
+using MHGameWork.TheWizards.Gameplay;
+using MHGameWork.TheWizards.RTSTestCase1.Inputting;
+using NUnit.Framework;
+using SlimDX;
+
+namespace MHGameWork.TheWizards.RTSTestCase1._Tests
+{
+    [TestFixture]
+    [EngineTest]
+    public class GoblinsTest
+    {
+        private TWEngine engine = EngineFactory.CreateEngine();
+
+        /// <summary>
+        /// This is a full testcase for the goblins functionality. Unit tests should be added later on when the testcase is completed.
+        /// </summary>
+        [Test]
+        public void TestAll()
+        {
+            
+        }
+    }
+}
