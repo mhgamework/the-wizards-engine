d84835b678a3006bc077cc63d43ece2f4d9751d0
MHGW-Laptop <mhgamework@gmail.com>
2013-05-24 21:47:26 +0200

    Marked PersistAttribute.cs as obsolete
    Former-commit-id: b50d706dde4df3d05ba5947b6ffb724972df0e6f

--- NewModules/Persistence/PersistAttribute.cs
+++ NewModules/Persistence/PersistAttribute.cs
@@ -8,7 +8,9 @@ namespace MHGameWork.TheWizards.Persistence
     /// <summary>
     /// This attribute is to be set on an IModelObject and notifies the system that objects
     /// of given type should be persisted to disk
+    /// This is part of the old system and should be revised
     /// </summary>
+    [Obsolete]
     public class PersistAttribute : Attribute
     {
     }
