24976e1c0922f91ebb0aa50e4f97b74a61d245f6
Jasper Hilven <jasperhilven@gmail.com>
2013-06-08 11:25:09 +0200

    Commit for push
    Former-commit-id: 76bd008ea538995a0c24296717707cde14a4a3b9

--- Gameplay/RTSTestCase1/Rendering/RTSEntitySimulator.cs
+++ Gameplay/RTSTestCase1/Rendering/RTSEntitySimulator.cs
@@ -1,5 +1,6 @@
 ﻿using MHGameWork.TheWizards.Engine;
 using MHGameWork.TheWizards.RTSTestCase1.Items;
+using MHGameWork.TheWizards.RTSTestCase1.Magic;
 using MHGameWork.TheWizards.RTSTestCase1.WorldResources;
 
 namespace MHGameWork.TheWizards.RTSTestCase1.Rendering
@@ -13,6 +14,9 @@ namespace MHGameWork.TheWizards.RTSTestCase1.Rendering
         {
             TW.Data.EnsureAttachment<Rock, RockRenderData>(o => new RockRenderData(o));
             foreach (Rock t in TW.Data.GetChangedObjects<Rock>()) t.get<RockRenderData>().Update();
+
+            TW.Data.EnsureAttachment<SimpleCrystal, CrystalRenderData>(o => new CrystalRenderData(o));
+            foreach (SimpleCrystal t in TW.Data.GetChangedObjects<SimpleCrystal>()) t.get<CrystalRenderData>().Update();
 
         }
 
