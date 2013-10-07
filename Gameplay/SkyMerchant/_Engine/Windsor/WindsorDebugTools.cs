2df85bec27cca1a3ebbaeb3c1d9a4dc7a750ab39
MHGameWorkBase <mhgamework@gmail.com>
2013-10-07 16:00:45 +0200

    Added text debugging feature for windsor
    Former-commit-id: 1253948221fa6239b4214560cbb6fbfbb9627759

---
+++ Gameplay/SkyMerchant/_Engine/Windsor/WindsorDebugTools.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Castle.Core;
+using Castle.Windsor;
+
+namespace MHGameWork.TheWizards.SkyMerchant._Engine.Windsor
+{
+    /// <summary>
+    /// Some debugging facilities for windsor. Currently logging to a string is supported
+    /// Currently tweaked for the skymerchant specifically :)
+    /// </summary>
+    public class WindsorDebugTools
+    {
+        private StringBuilder builder = new StringBuilder();
+        /// <summary>
+        /// Generates a string of all components, their lifestyle and their services
+        /// </summary>
+        /// <returns></returns>
+        public string GenerateDependenciesString(IWindsorContainer container)
+        {
+            builder.Clear();
+            foreach (var node in container.Kernel.GraphNodes.Cast<ComponentModel>())
+            {
+                write(node.Name + " : " + node.LifestyleType);
+
+                foreach (var s in node.Services) write(" -- " + s);
+            }
+            return builder.ToString();
+        }
+
+        private void write(string line)
+        {
+            builder.AppendLine(line
+                .Replace("MHGameWork.TheWizards.SkyMerchant", "SKY")
+                .Replace("MHGameWork.TheWizards", "TW"));
+        }
+    }
+}
---
+++ Gameplay/SkyMerchant/_Tests/Development/WindsorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Castle.Windsor;
+using MHGameWork.TheWizards.SkyMerchant.Prototype;
+using MHGameWork.TheWizards.SkyMerchant._Engine.Windsor;
+using NUnit.Framework;
+
+namespace MHGameWork.TheWizards.SkyMerchant._Tests.Development
+{
+    /// <summary>
+    /// Test some of the windsor utilities
+    /// </summary>
+    [TestFixture]
+    public class WindsorTest
+    {
+        [Test]
+        public void TestDebugWriting()
+        {
+            var container = new WindsorContainer();
+            container.Install(new PrototypeInstaller());
+
+            var tools = new WindsorDebugTools();
+            var str = tools.GenerateDependenciesString(container); //.Mark();
+
+            Console.WriteLine(str);
+        }
+    }
+}
