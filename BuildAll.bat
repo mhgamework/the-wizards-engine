32b5788eb6de4716c1e0646f621bd1557e2f29eb
MHGameWorkBase <mhgamework@gmail.com>
2013-10-21 11:55:42 +0200

    Added copy from bin/Binaries to bin/BinariesGmae to BuildAll.bat
    Former-commit-id: d6068b717d929619f168773438e1f6f217c3fb33

--- BuildAll.bat
+++ BuildAll.bat
@@ -1,3 +1,4 @@
 cd _scripts
 call clean.bat
 "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "The Wizards.sln" /p:Configuration=Debug /p:Platform=x86
+xcopy "bin/Binaries" "bin/BinariesGame" /y
