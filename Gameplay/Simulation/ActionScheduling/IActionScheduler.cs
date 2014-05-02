db605b9bd65e38981617e4138164ffcb9b059039
Michiel Huygen <mhgamework@gmail.com>
2014-05-02 16:49:21 +0200

    Added IActionScheduler + test
    Former-commit-id: 9b1bb36971021ecac3cc87318ac26068ff635d19

---
+++ Gameplay/Simulation/ActionScheduling/IActionScheduler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MHGameWork.TheWizards.Simulation.ActionScheduling
+{
+    /// <summary>
+    /// Gameloop-based action scheduler and executor
+    /// TODO: create optimized implementation
+    /// </summary>
+    public class IActionScheduler
+    {
+        private List<ScheduledAction> actions = new List<ScheduledAction>();
+
+        private float currentTime;
+
+        public void SetTimeout(float interval, Action action)
+        {
+            actions.Add(new ScheduledAction(currentTime + interval, action));
+        }
+
+        public void SetCurrentTime(float newTime)
+        {
+            currentTime = newTime;
+        }
+
+        public void ExecuteActions(float newTime)
+        {
+            var expiredActions = actions.Where(a => a.Time <= newTime).ToArray();
+
+            currentTime = newTime;
+
+
+            foreach (var a in expiredActions)
+            {
+                actions.Remove(a);
+                a.Action();
+            }
+
+        }
+
+        private class ScheduledAction
+        {
+            public float Time;
+            public Action Action;
+
+            public ScheduledAction(float time, Action action)
+            {
+                Time = time;
+                Action = action;
+            }
+        }
+    }
+}
---
+++ Gameplay/_Tests/Simulation/ActionSchedulerTest.cs
@@ -0,0 +1,62 @@
+﻿using MHGameWork.TheWizards.Simulation.ActionScheduling;
+using NUnit.Framework;
+
+namespace MHGameWork.TheWizards._Tests.Simulation
+{
+    [TestFixture]
+    public class ActionSchedulerTest
+    {
+        [Test]
+        public void TestSchedule()
+        {
+            var scheduler = new IActionScheduler();
+
+            var value = 0;
+
+
+            scheduler.SetCurrentTime(0);
+
+
+            scheduler.SetTimeout(2, () => value = 5);
+            scheduler.SetTimeout(3, () => value = 3);
+            scheduler.SetTimeout(6, () => value++);
+            scheduler.SetTimeout(6, () => value++);
+            scheduler.SetTimeout(8, () =>
+            {
+                value = 10;
+                scheduler.SetTimeout(1, () => value++);
+            });
+
+
+            scheduler.ExecuteActions(1);
+            Assert.AreEqual(0, value);
+
+            scheduler.ExecuteActions(1.5f);
+            Assert.AreEqual(0, value);
+
+            scheduler.ExecuteActions(2);
+            Assert.AreEqual(5, value);
+
+            scheduler.ExecuteActions(3);
+            Assert.AreEqual(3, value);
+
+            scheduler.ExecuteActions(4);
+            Assert.AreEqual(3, value);
+
+            scheduler.ExecuteActions(6);
+            Assert.AreEqual(5, value);
+
+            scheduler.ExecuteActions(6.5f);
+            Assert.AreEqual(5, value);
+
+            scheduler.ExecuteActions(8);
+            Assert.AreEqual(10, value);
+
+            scheduler.ExecuteActions(8.5f);
+            Assert.AreEqual(10, value);
+
+            scheduler.ExecuteActions(9);
+            Assert.AreEqual(11, value);
+        }
+    }
+}
