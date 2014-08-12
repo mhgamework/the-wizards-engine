19585b81f3b91ad4aabe6b9d145a2fd05bba1a10
MHGW-Laptop <mhgamework@gmail.com>
2014-08-12 12:06:51 +0200

    Refactored out some networking utilities from networking unit tests
    Former-commit-id: fac1c6c46c59b3f0d895809f34b6990067cd394b

---
+++ Common.Core/Networking/NetworkingUtilities.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace MHGameWork.TheWizards.Networking
+{
+    public class NetworkingUtilities
+    {
+        public static void EstablishTwoWayTCP(out TCPConnection conn1, out  TCPConnection conn2)
+        {
+            TCPConnectionListener listener = new TCPConnectionListener(10010);
+            conn2 = null;
+
+            TCPConnection connected = null;
+
+            AutoResetEvent ev = new AutoResetEvent(false);
+
+            listener.ClientConnected += delegate(object sender, TCPConnectionListener.ClientConnectedEventArgs e)
+            {
+                connected = new TCPConnection(e.CL);
+                ev.Set();
+            };
+
+
+            listener.Listening = true;
+
+            Thread.Sleep(500);
+
+            conn1 = ConnectTCP(10010, "127.0.0.1");
+
+
+            ev.WaitOne();
+
+            conn2 = connected;
+
+
+
+            listener.Listening = false;
+            listener.Dispose();
+        }
+        public static TCPConnection ConnectTCP(int port, string ip)
+        {
+            AutoResetEvent ev = new AutoResetEvent(false);
+
+            var conn = new TCPConnection();
+            conn.ConnectedToServer += delegate { ev.Set(); };
+
+            conn.Connect(ip, port);
+            if (!ev.WaitOne(5000)) throw new Exception("Connection timed out!");
+
+            return conn;
+        }
+    }
+}
--- Unit Tests/Features/Core/Networking/NetworkingClientTest.cs
+++ Unit Tests/Features/Core/Networking/NetworkingClientTest.cs
@@ -12,48 +12,12 @@ namespace MHGameWork.TheWizards.Tests.Features.Core.Networking
 
         private void establishTCP(out TCPConnection conn1, out  TCPConnection conn2)
         {
-            TCPConnectionListener listener = new TCPConnectionListener(10010);
-            conn2 = null;
-
-            TCPConnection connected = null;
-
-            AutoResetEvent ev = new AutoResetEvent(false);
-
-            listener.ClientConnected += delegate(object sender, TCPConnectionListener.ClientConnectedEventArgs e)
-            {
-                connected = new TCPConnection(e.CL);
-                ev.Set();
-            };
-
-
-            listener.Listening = true;
-
-            Thread.Sleep(500);
-
-            conn1 = ConnectTCP(10010, "127.0.0.1");
-
-
-            ev.WaitOne();
-
-            conn2 = connected;
-
-
-
-            listener.Listening = false;
-            listener.Dispose();
+            NetworkingUtilities.EstablishTwoWayTCP(out conn1, out conn2);
         }
 
         public static TCPConnection ConnectTCP(int port, string ip)
         {
-            AutoResetEvent ev = new AutoResetEvent(false);
-
-            var conn = new TCPConnection();
-            conn.ConnectedToServer += delegate { ev.Set(); };
-
-            conn.Connect(ip, port);
-            if (!ev.WaitOne(5000)) throw new Exception("Connection timed out!");
-
-            return conn;
+            return NetworkingUtilities.ConnectTCP(port, ip);
         }
 
 
