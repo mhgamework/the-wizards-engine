8ad59abaceabf0031ef53c1f7c64baa5c64764dc
Simon Chuptys <simon.chuptys@gmail.com>
2014-08-16 16:51:43 +0200

    Dataval updating for woodworker.
    Former-commit-id: 746f685ea163d1f733252eb88a2f33c27e1eb0e1

--- Gameplay/GodGame/Types/WoodworkerType.cs
+++ Gameplay/GodGame/Types/WoodworkerType.cs
@@ -10,6 +10,7 @@ namespace MHGameWork.TheWizards.GodGame.Types
     public class WoodworkerType : GameVoxelType
     {
         private int harvestRange = 5;
+        private int inventorySize = 5;
 
         public WoodworkerType()
             : base("Woodworker")
@@ -19,9 +20,17 @@ namespace MHGameWork.TheWizards.GodGame.Types
 
         public override void Tick(IVoxelHandle handle)
         {
-            handle.Data.Inventory.ChangeCapacity(5);
-            handle.EachRandomInterval(2f, () => tryHarvest(handle));
-            handle.EachRandomInterval(2f, () => tryOutput(handle));
+            handle.Data.Inventory.ChangeCapacity(inventorySize);
+            handle.EachRandomInterval(2f, () =>
+            {
+                tryHarvest(handle);
+                updateDataval(handle);
+            });
+            handle.EachRandomInterval(2f, () =>
+                {
+                    tryOutput(handle);
+                    updateDataval(handle);
+                });
         }
 
         private void tryHarvest(IVoxelHandle handle)
@@ -39,19 +48,34 @@ namespace MHGameWork.TheWizards.GodGame.Types
 
         private void tryOutput(IVoxelHandle handle)
         {
+            if (handle.Data.Inventory.GetAmountOfType(Forest.GetWoodItemType()) < inventorySize) return;
+
             var itemType = Forest.GetWoodItemType();
             if (handle.Data.Inventory[itemType] == 0) return;
 
             var target = handle.Get4Connected().FirstOrDefault(v => v.CanAcceptItemType(itemType));
             if (target == null || !target.CanAcceptItemType(itemType)) return;
 
-            if (target.Type is RoadType)
-                Road.DeliverItemClosest(target, handle, itemType);
+            for (int i = 0; i < inventorySize; i++)
+            {
+                if (handle.Data.Inventory[itemType] == 0) break;
+                if (target.Type is RoadType)
+                    Road.DeliverItemClosest(target, handle, itemType);
+            }
+
+
         }
 
         private IEnumerable<IVoxelHandle> getHarvestableVoxels(IVoxelHandle handle)
         {
             return handle.GetRange(harvestRange).Where(e => e.Type is ForestType);
+        }
+
+        private void updateDataval(IVoxelHandle handle)
+        {
+            var nbItems = handle.Data.Inventory.GetAmountOfType(Forest.GetWoodItemType());
+            var dataVal = nbItems > 3 ? 3 : nbItems;
+            handle.Data.DataValue = dataVal;
         }
 
         public override IEnumerable<IVoxelInfoVisualizer> GetInfoVisualizers(IVoxelHandle handle)
