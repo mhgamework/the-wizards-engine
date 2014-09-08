aa0de8224d7456c80e48bb3f4a50506c7d6a358f
Michiel Huygen <mhgamework@gmail.com>
2014-09-08 10:42:34 +0200

    Refactored the textarea updaters
    Former-commit-id: 13cc634febfabfb7784a6ea961af9d76b8c1b5b7

---
+++ Gameplay/Engine/WorldRendering/FastTextareaUpdater.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+namespace MHGameWork.TheWizards.Engine.WorldRendering
+{
+    /// <summary>
+    /// Responsible for processing changes to a textarea
+    /// Uses the DX11FontWrapper for rendering: fast!
+    /// </summary>
+    public class FastTextareaUpdater : ITextareaUpdater
+    {
+        private DX11FontWrapper fontWrapper;
+
+        public FastTextareaUpdater()
+        {
+            fontWrapper = new DX11FontWrapper(TW.Graphics.Device,"Verdana");
+        }
+
+        public void Simulate()
+        {
+            render();
+        }
+
+        private void render()
+        {
+            TW.Graphics.Device.ImmediateContext.OutputMerger.BlendState = TW.Graphics.HelperStates.AlphaBlend;
+            foreach (var a in TW.Data.Objects.OfType<Textarea>())
+            {
+                if (!a.Visible) continue;
+
+                var pxSize = a.FontSize/12f*16f;
+
+                TW.Graphics.TextureRenderer.DrawColor(a.BackgroundColor, a.Position, a.Size);
+                fontWrapper.Draw(a.Text, pxSize , (int)a.Position.X, (int)a.Position.Y, a.Color);
+            }
+        }
+
+    }
+}
--- Gameplay/Engine/WorldRendering/SlowTextareaUpdater.cs
+++ Gameplay/Engine/WorldRendering/SlowTextareaUpdater.cs
@@ -1,48 +1,9 @@
 ﻿using System.Collections.Generic;
 using MHGameWork.TheWizards.Rendering.Text;
 using SlimDX;
-using System.Linq;
 
 namespace MHGameWork.TheWizards.Engine.WorldRendering
 {
-    public interface ITextareaUpdater : ISimulator
-    {
-    }
-
-    /// <summary>
-    /// Responsible for processing changes to a textarea
-    /// Uses the DX11FontWrapper for rendering: fast!
-    /// </summary>
-    public class FastTextareaUpdater : ITextareaUpdater
-    {
-        private DX11FontWrapper fontWrapper;
-
-        public FastTextareaUpdater()
-        {
-            fontWrapper = new DX11FontWrapper(TW.Graphics.Device,"Verdana");
-        }
-
-        public void Simulate()
-        {
-            render();
-        }
-
-        private void render()
-        {
-            TW.Graphics.Device.ImmediateContext.OutputMerger.BlendState = TW.Graphics.HelperStates.AlphaBlend;
-            foreach (var a in TW.Data.Objects.OfType<Textarea>())
-            {
-                if (!a.Visible) continue;
-
-                var pxSize = a.FontSize/12f*16f;
-
-                TW.Graphics.TextureRenderer.DrawColor(a.BackgroundColor, a.Position, a.Size);
-                fontWrapper.Draw(a.Text, pxSize , (int)a.Position.X, (int)a.Position.Y, a.Color);
-            }
-        }
-
-    }
-
     /// <summary>
     /// Responsible for processing changes to a textarea
     /// Uses the TextTexture: GDI->disk->D3D == slow
