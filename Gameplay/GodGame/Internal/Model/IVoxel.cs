8656c30e5c02d5175f7ba6d799affd0ccdb06e8d
Michiel Huygen <mhgamework@gmail.com>
2014-09-25 11:32:53 +0200

    Some cleanup for IVoxel
    Former-commit-id: 903d0f7b30c19f5ed8df9a58060d7eeb088789c0

---
+++ Gameplay/GodGame/Internal/Model/IVoxel.cs
@@ -0,0 +1,29 @@
+﻿using DirectX11;
+
+namespace MHGameWork.TheWizards.GodGame.Internal.Model
+{
+    /// <summary>
+    /// Represents a voxel of the world
+    /// NOTE/IDEA: if the amount of instances of IVoxel causes memory problems,
+    ///    this interface can be implemented by a struct with only a point2
+    ///    if the struct contains an additonal reference to some sort of repository
+    ///    holding the actual data
+    /// </summary>
+    public interface IVoxel
+    {
+        /// <summary>
+        /// Returns the voxel at a relative position offset with respect to this
+        /// </summary>
+        /// <param name="offset"></param>
+        /// <returns></returns>
+        IVoxel GetRelative(Point2 offset);
+        /// <summary>
+        /// Returns the position of other relative to this
+        /// </summary>
+        Point2 GetOffset(IVoxel other);
+        IVoxelData Data { get; }
+        /*ITileType Type { get; set; }
+        ITile Tile { get; set; }
+        float Height { get; set; }*/
+    }
+}
--- Gameplay/GodGame/Internal/Model/VoxelSpatialExtensions.cs
+++ Gameplay/GodGame/Internal/Model/VoxelSpatialExtensions.cs
@@ -1,45 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using DirectX11;
-using MHGameWork.TheWizards.GodGame.Types;
-using StillDesign.PhysX;
 
 namespace MHGameWork.TheWizards.GodGame.Internal.Model
 {
     /// <summary>
-    /// Represents the contents of a voxel 
-    /// Corresponds to a GameVoxelType
+    /// Provides utility methods for IVoxel spatial operations
     /// </summary>
-    public interface ITile : IDisposable
-    {
-
-    }
-
-    /// <summary>
-    /// Represents a voxel of the world
-    /// NOTE/IDEA: if the amount of instances of IVoxel causes memory problems,
-    ///    this interface can be implemented by a struct with only a point2
-    ///    if the struct contains an additonal reference to some sort of repository
-    ///    holding the actual data
-    /// </summary>
-    public interface IVoxel
-    {
-        /// <summary>
-        /// Returns the voxel at a relative position offset with respect to this
-        /// </summary>
-        /// <param name="offset"></param>
-        /// <returns></returns>
-        IVoxel GetRelative(Point2 offset);
-        /// <summary>
-        /// Returns the position of other relative to this
-        /// </summary>
-        Point2 GetOffset(IVoxel other);
-        IVoxelData Data { get; }
-        /*ITileType Type { get; set; }
-        ITile Tile { get; set; }
-        float Height { get; set; }*/
-    }
-
     public static class VoxelSpatialExtensions
     {
         public static IEnumerable<IVoxel> Get8Connected(this IVoxel v)
