9e5e8f1a216815f4714b3ca3e20a4048c36b43dd
Michiel Huygen <mhgamework@gmail.com>
2014-09-30 15:55:43 +0200

    Note on IVoxelData
    Former-commit-id: 1bf80e54302f67537d4ec9d089e00f93f0043e4b

--- Gameplay/GodGame/Model/IVoxelData.cs
+++ Gameplay/GodGame/Model/IVoxelData.cs
@@ -8,6 +8,9 @@ namespace MHGameWork.TheWizards.GodGame
 {
     /// <summary>
     /// Provides the data storage for a single voxel
+    /// TODO: Voxel data is actually not defined by some stupid gameplay fields
+    ///     It is defined by the fact that it is observable data, and that it is network and disk serializable.
+    ///     The actual gameplay fields are not part of the engine subsystem, but of the gameplay system
     /// </summary>
     public interface IVoxelData
     {
