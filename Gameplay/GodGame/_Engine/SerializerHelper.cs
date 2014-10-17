5f374c4ce5d14c68d1ac1ab623bac927080a2566
Michiel Huygen <michiel.huygen@cs.kuleuven.be>
2014-10-17 15:00:14 +0200

    Added Utilities project for non-TW related c# logic.
    Conflicts:
    	Utilities/Reflection/InternalReflectionExtensions.cs
    
    Former-commit-id: 07f8e6411f98c46f49639f18f1f07d76881b4a67

--- Gameplay/GodGame/_Engine/SerializerHelper.cs
+++ Gameplay/GodGame/_Engine/SerializerHelper.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
-using MHGameWork.TheWizards.GodGame.Persistence;
 
 namespace MHGameWork.TheWizards.GodGame._Engine
 {
---
+++ Utilities/Reflection/InternalReflectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace MHGameWork.TheWizards.GodGame._Engine
+{
+    public static class InternalReflectionExtensions
+    {
+        public static T GetInternalField<T>(this object obj, string name)
+        {
+            return (T)obj.GetType().GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance).GetValue(obj);
+        }
+
+        public static T CallInternalMethod<T>(this object obj, string method)
+        {
+            return (T)obj.GetType().GetMethod(method, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance).Invoke(obj, new object[] { });
+        }
+    }
+}
