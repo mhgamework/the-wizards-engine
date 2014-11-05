c0b1d740098da24cef2cb0b1250d9df545675431
Michiel Huygen <michiel.huygen@cs.kuleuven.be>
2014-11-05 13:40:07 +0100

    Recursion utilities
    Former-commit-id: 522376dda08e7eaeff5d09a8ae084bf01995eb0b

---
+++ Utilities/RecursionExtensions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjectVersioning
+{
+    public static class RecursionExtensions
+    {
+        /// <summary>
+        /// Walks to a tree depth first.
+        /// Does not return the root
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="start"></param>
+        /// <param name="getChildren"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> DepthFirst<T>(this T root, Func<T, IEnumerable<T>> getChildren) where T : class
+        {
+            var stack = new Stack<IEnumerator<T>>();
+
+            stack.Push(getChildren(root).GetEnumerator());
+            while (stack.Count > 0)
+            {
+                var en = stack.Peek();
+
+                if (!en.MoveNext())
+                {
+                    // Empty
+                    stack.Pop();
+                    continue;
+                }
+
+                var el = en.Current;
+
+                yield return el;
+
+                stack.Push(getChildren(el).GetEnumerator());
+            }
+        }
+
+        /// <summary>
+        /// Walks to a tree breadth first.
+        /// Does not return the root
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="start"></param>
+        /// <param name="getChildren"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> BreadthFirst<T>(this T root, Func<T, IEnumerable<T>> getChildren) where T : class
+        {
+            var stack = new Queue<IEnumerable<T>>();
+
+            stack.Enqueue(getChildren(root));
+            while (stack.Count > 0)
+            {
+                var el = stack.Dequeue();
+                foreach (var c in el)
+                {
+                    yield return c;
+                    stack.Enqueue(getChildren(c));
+                }
+            }
+        }
+
+        public static T Root<T>(this T child, Func<T, T> getParent)
+        {
+            while (getParent(child) != null) child = getParent(child);
+            return child;
+        }
+
+        /// <summary>
+        /// NOTE for cleanup: this is actually thesame as depthfirst or depthfirst for a child collection of 1 element
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="child"></param>
+        /// <param name="getParent"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Parents<T>(this T child, Func<T, T> getParent) where T : class
+        {
+            while (getParent(child) != null)
+            {
+                child = getParent(child);
+                yield return child;
+            }
+        }
+    }
+}
---
+++ Utilities/RecursionExtensionsTest.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+
+namespace ObjectVersioning.Tests
+{
+    /*[TestFixture]
+    public class RecursionExtensionsTest
+    {
+        private Node r;
+        private Node ra;
+        private Node rb;
+        private Node raa;
+        private Node rba;
+        private Node rbb;
+
+        [SetUp]
+        public void Setup()
+        {
+            r = new Node();
+            ra = new Node();
+            rb = new Node();
+            raa = new Node();
+            rba = new Node();
+            rbb = new Node();
+
+
+            r.AddChild(ra);
+            r.AddChild(rb);
+
+            ra.AddChild(raa);
+
+            rb.AddChild(rba);
+            rb.AddChild(rbb);
+        }
+
+        [Test]
+        public void TestDepthFirst()
+        {
+            CollectionAssert.AreEqual(new[] { ra, raa, rb, rba, rbb }, r.DepthFirst(c => c.Children));
+            CollectionAssert.AreEqual(new[] { raa }, ra.DepthFirst(c => c.Children));
+            CollectionAssert.AreEqual(new[] { rba, rbb }, rb.DepthFirst(c => c.Children));
+
+        }
+
+        [Test]
+        public void TestBreadthFirst()
+        {
+            CollectionAssert.AreEqual(new[] { ra, rb, raa, rba, rbb }, r.BreadthFirst(c => c.Children).PrintArray());
+            CollectionAssert.AreEqual(new[] { raa }, ra.BreadthFirst(c => c.Children));
+            CollectionAssert.AreEqual(new[] { rba, rbb }, rb.BreadthFirst(c => c.Children));
+        }
+
+        [Test]
+        public void TestRoot()
+        {
+            Assert.AreEqual(r, r.Root(c => c.Parent));
+            Assert.AreEqual(r, ra.Root(c => c.Parent));
+            Assert.AreEqual(r, rb.Root(c => c.Parent));
+            Assert.AreEqual(r, raa.Root(c => c.Parent));
+            Assert.AreEqual(r, rba.Root(c => c.Parent));
+            Assert.AreEqual(r, rbb.Root(c => c.Parent));
+        }
+
+
+        public class Node
+        {
+            public static int nextId = 0;
+            private int id;
+            public Node()
+            {
+                id = nextId++;
+            }
+
+            public List<Node> Children = new List<Node>();
+            public Node Parent;
+
+            public override string ToString()
+            {
+                return string.Format("Id: {0}", id);
+            }
+
+            public void AddChild(Node child)
+            {
+                Children.Add(child);
+                child.Parent = this;
+            }
+        }
+    }*/
+}
