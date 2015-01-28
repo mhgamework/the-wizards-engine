971caa7d17a7e4c93a72a5f8a2d18c9ebc5db123
Michiel Huygen <michiel.huygen@cs.kuleuven.be>
2015-01-28 16:58:38 +0100

    Some preliminary refactorings
    Conflicts:
    	Gameplay/DualContouring/DualContouringTest.cs
    	Gameplay/DualContouring/HermiteDataGrid.cs
    	Gameplay/DualContouring/QEFCalculator.cs
    
    Former-commit-id: b5ec05d183dbab257ee9e4b5d7fefbf8bfa92b1c

---
+++ Gameplay/DualContouring/IIntersectableCube.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using SlimDX;
+using ContainmentType = Microsoft.Xna.Framework.ContainmentType;
+
+namespace MHGameWork.TheWizards.DualContouring
+{
+    public class IntersectableCube : IIntersectableObject
+    {
+        private Vector3 cubeCenter;
+        private float cubeRadius;
+
+        public bool IsInside(Vector3 v)
+        {
+            var bb = new BoundingBox(cubeCenter - new Vector3(cubeRadius), cubeCenter + new Vector3(cubeRadius));
+            return bb.xna().Contains(v.xna()) != ContainmentType.Contains;
+        }
+
+        public Vector4 GetIntersection(Vector3 start, Vector3 end)
+        {
+            start -= cubeCenter;
+            end -= cubeCenter;
+            // find which edge we are at
+            var dirs = new[] { Vector3.UnitX, Vector3.UnitY, Vector3.UnitZ };
+            foreach (var dir in dirs)
+            {
+                var e = Vector3.Dot(end, dir);
+                var s = Vector3.Dot(start, dir);
+                if (e > cubeRadius)
+                    return new Vector4(dir, (cubeRadius - s) / (e - s));
+                if (s < -cubeRadius)
+                    return new Vector4(-dir, (-cubeRadius - s) / (e - s));
+            }
+            throw new InvalidOperationException("Not a crossing edge!");
+        }
+    }
+}
---
+++ Gameplay/DualContouring/IntersectableSphere.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DirectX11;
+using SlimDX;
+
+namespace MHGameWork.TheWizards.DualContouring
+{
+    public class IntersectableSphere :IIntersectableObject
+    {
+        public bool IsInside(Vector3 v)
+        {
+            v -= 5 * MathHelper.One;
+            return v.Length() > 4;
+        }
+
+        public Vector4 GetIntersection(Vector3 start, Vector3 end)
+        {
+            var ray = new Ray(start, Vector3.Normalize(end - start));
+            var sphere = new BoundingSphere(new Vector3(5), 4);
+            float? intersect;
+            intersect = ray.xna().Intersects(sphere.xna());
+            if (!intersect.HasValue || intersect.Value < 0.001 || intersect.Value > (end - start).Length() + 0.0001)
+            {
+
+                //Try if inside of sphere   
+                ray = new Ray(end, Vector3.Normalize(start - end));
+                intersect = ray.xna().Intersects(sphere.xna());
+
+                if (!intersect.HasValue || intersect.Value < -0.001 || intersect.Value > (end - start).Length() + 0.0001)
+                    throw new InvalidOperationException();
+                intersect = (start - end).Length() - intersect.Value;
+                ray = new Ray(start, Vector3.Normalize(end - start));
+
+            }
+
+            var pos = ray.GetPoint(intersect.Value);
+
+            return new Vector4(Vector3.Normalize(pos - new Vector3(5)), (pos - start).Length() / (end - start).Length());
+        }
+    }
+}
