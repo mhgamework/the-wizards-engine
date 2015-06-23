67d382c70bd8b237b722d3c9ead593632b292ca3
Michiel Huygen <mhgamework@gmail.com>
2015-06-23 17:14:30 +0200

    Added PrettyPrintExtensions.cs, for timespan

---
+++ Gameplay/DualContouring/PrettyPrintExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MHGameWork.TheWizards.DualContouring
+{
+    /// <summary>
+    /// Several usefull extension for pretty printing 
+    /// </summary>
+    public static class PrettyPrintExtensions
+    {
+        public static string PrettyPrint(this TimeSpan timeSpan)
+        {
+            if (timeSpan.TotalSeconds > 1000) throw new InvalidOperationException("Only for less than 1000 second intervals(more not yet implemented)");
+            var secs = timeSpan.TotalSeconds;
+            if (secs > 1)
+                return string.Format("{0:0.00} sec", secs);
+            secs *= 1000;
+            if (secs > 1)
+                return string.Format("{0:0.00} ms", secs);
+            secs *= 1000;
+            return string.Format("{0:0.00} µs", secs);
+
+        }
+    }
+}
