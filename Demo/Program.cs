d89c0cf94e0ef8e624ac5750acb5b99a80c9f9f2
Michiel Huygen <mhgamework@gmail.com>
2015-08-14 17:41:17 +0200

    Created Demo project for running standalone demos

---
+++ Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MHGameWork.TheWizards.VoxelEngine;
+
+namespace Demo
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var test = new DualContouringBuilderTest();
+            test.InitializeEngine();
+            test.Setup();
+            test.Run();
+            test.RunEngine();
+            
+        }
+    }
+}
