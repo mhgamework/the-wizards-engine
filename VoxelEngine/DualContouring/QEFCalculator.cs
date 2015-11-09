7008933f59bd19203c2ced0dccbcfed87428ef21
Michiel Huygen <mhgamework@gmail.com>
2015-11-09 20:45:02 +0100

    Added iterative QEF solver

--- VoxelEngine/DualContouring/QEFCalculator.cs
+++ VoxelEngine/DualContouring/QEFCalculator.cs
@@ -88,6 +88,56 @@ namespace MHGameWork.TheWizards.DualContouring
 
             return V.Multiply(S.Inverse()).Multiply(U1.Transpose().Multiply(b));*/
         }
+
+
+        /// <summary>
+        ///     WARNING MIGHT NOT BE THREAD SAFE
+        /// </summary>
+        /// <param name="normals"></param>
+        /// <param name="posses"></param>
+        /// <param name="preferredPosition"></param>
+        /// <returns></returns>
+        public static Vector3 CalculateCubeQEFIteratively(Vector3[] normals, Vector3[] posses,
+            int numIntersections, Vector3 preferredPosition)
+        {
+
+            var curr = preferredPosition;
+            var err = 0f;
+
+            for (int j = 0; j < 100; j++)
+            {
+                err = 0;
+                //Debug.Log("Start");
+                var meanOnplane = new Vector3();
+
+                for (int i = 0; i < numIntersections; i++)
+                {
+                    // Project on plane
+                    var planePos = posses[i];
+                    var planeN = normals[i];
+                    var relCurr = curr - planePos;
+                    var offsetAlongNormal = Vector3.Dot(relCurr, planeN);
+                    var onPlane = curr + offsetAlongNormal * -planeN;
+                    err += (onPlane - curr).LengthSquared();
+                    meanOnplane += onPlane;
+                }
+                if (err < 0.0001f) break;
+
+                //Debug.Log(err);
+                meanOnplane /= numIntersections;
+                curr += (meanOnplane - curr) * 0.7f; // Found this number on some blogpost
+                //curr = new Vector3(0,0,0);
+
+            }
+            if (err >= 0.001f)
+            {
+                int a = 5;
+            }
+
+
+            return curr;
+        }
+
         /// <summary> 
         /// Moore–Penrose pseudoinverse 
         /// If A = U • Σ • VT is the singular value decomposition of A, then A† = V • Σ† • UT. 
